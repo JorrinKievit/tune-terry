da9111747c57c9d804897004183d327204b3eb66
Jorrin <jorrinkievit@hotmail.com>
2023-09-27 00:01:18 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { LogLevel, SapphireClient } from "@sapphire/framework";
 import { GatewayIntentBits, Partials } from "discord.js";
 import { config } from "dotenv";
+import express from "express";
 
 config();
+
+const app = express(); // Create an Express app
+
+// Define a simple health check endpoint
+app.get("/health", (request, res) => {
+  res.status(200).send("healthy");
+});
+
+app.listen(3000);
 
 const client = new SapphireClient({
   defaultPrefix: "!tt",
